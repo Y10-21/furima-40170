7c0ed3944213c215d6630882872d894ea03dfd81
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-01-19 12:02:46 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :email
-    validates :encrypted_password
     validates :name_first
     validates :name_secound
     validates :name_first_kana
     validates :name_secound_kana
     validates :birthday
   end
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
-  validates :name_first, format: { with: /\A[^\p{ASCII}]+\z/}
-  validates :name_secound, format: { with: /\A[^\p{ASCII}]+\z/}
-  validates :name_first_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  validates :name_secound_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d ])[a-z\d]+\z/i }, on: :create
+  validates :name_first, format: { with: /\A[^\p{ASCII}]+\z/ }
+  validates :name_secound, format: { with: /\A[^\p{ASCII}]+\z/ }
+  validates :name_first_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  validates :name_secound_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
 end

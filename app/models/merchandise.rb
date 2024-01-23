188aebaa24c923c46c896a533576ca2bb33ed135
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-01-23 17:29:41 +0900

    バイデーション

--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -6,11 +6,20 @@ class Merchandise < ApplicationRecord
     self.image.attached?
   end
 
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+  end
+
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_load_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :price, inclusion: { in: 300..9999999 }
+  validates :price,format:{ with: /\A[0-9]+\z/ }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category

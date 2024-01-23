0d3e7c23e8cb0757335a1b4754f2b2b4bc0126ba
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-01-23 16:40:38 +0900

    販売価格表記

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,1 +1,20 @@
-console.log(1);
+document.addEventListener('DOMContentLoaded', function () {
+  const ContentPrice = document.getElementById('item-price');
+  const ContentCommission = document.getElementById('add-tax-price');
+  const ContentProfit =document.getElementById('profit');
+
+  if (!ContentPrice) {
+    return false;
+  }
+
+  ContentPrice.addEventListener('input', function () {
+    const price = Math.floor(ContentPrice.value);
+    const price_commission = Math.floor(price * 0.1)
+    const price_profit = price - price_commission
+
+    ContentPrice.innerHTML = price
+    ContentCommission.innerHTML = price_commission
+    ContentProfit.innerHTML = price_profit
+  });
+
+});

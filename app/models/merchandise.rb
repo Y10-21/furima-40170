7449f2133caf68d5f6682677846a918df685e573
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-01-24 18:27:43 +0900

    モデルテストコード記述

--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -2,15 +2,16 @@ class Merchandise < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  def was_attached
-    self.image.attached?
-  end
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :explanation
     validates :price
   end
+
+  validates :name ,length: { minimum: 1, maximum: 40 }
+  validates :explanation, length: { minimum: 1, maximum: 1000 }
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}

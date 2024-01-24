f6f829b8a33656bc426e9a9b802dfa936776b77a
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-01-24 17:02:15 +0900

    ファクトリーボットで商品生成

--- spec/models/merchandise_spec.rb
+++ spec/models/merchandise_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Merchandise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @merchandise = FactoryBot.build(:merchandise)
+  end
+
 end

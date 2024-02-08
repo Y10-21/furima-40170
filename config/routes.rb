d376f14692e0740b926b918b3d84b9d337a6be5d
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-02-08 18:12:30 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'merchandises#index'
   resources :merchandises
+  resources :merchandises, only: :purchase do
+    post 'purchase', on: :member
+  end
+  resources :merchandises do
+    resources :purchases 
+  end
 end

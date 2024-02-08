e72cdabc557f535217aa597ef32d2fb2e3ed70b0
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-02-08 18:58:48 +0900

    購入商品を表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :set_merchandise
+
   def index
+    # @purchase = @merchandise.purchases
+    @purchase = @merchandise
+  end
+
+  private
+  def set_merchandise
+    @merchandise = Merchandise.find(params[:merchandise_id])
   end
 end

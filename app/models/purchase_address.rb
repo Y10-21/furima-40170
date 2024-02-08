22559dc7442e48978cc04059f963d981289b6305
Y10-21 <rstoyo.yuto.hoshino@gmail.com>
2024-02-08 16:14:55 +0900

    Formオブジェクトのインスタンスを生成する記述

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,20 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga...
+  attr_accessor :user_id, :merchandise_id, :post_number, :prefecture_id, :post_city ,:post_address, :post_building, :telephone_number
 
   # ここにバリデーションの処理を書く
-
+  with_options presence: true do
+    validates :merchandise_id
+    validates :user_id
+    validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :post_city
+    validates :post_address
+    validates :telephone_number, numericality: { only_integer: true }, length: { is: 10 }
+  end
   def save
     # 各テーブルにデータを保存する処理を書く
+    purchase = Purchase.create(merchandise_id: merchandise_id, user_id: user_id)
+    Adress.create(post_number: post_number, prefecture_id: prefecture_id, post_city: post_city, post_address: post_address, telephone_number: telephone_number ,purchase_id: purchase_id)
   end
 end
